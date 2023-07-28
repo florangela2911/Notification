db57343677113c66a96e8b7e47ae3d4849774a0c
Diego Ojeda <daom89@gmail.com>
2023-07-28 16:38:12 -0500

    Cambio en vista de email

--- resources/views/mail/expenseReport.blade.php
+++ resources/views/mail/expenseReport.blade.php
@@ -1,30 +1,30 @@
-<x-app-layout>
-    <div class="py-4">
+<div class="py-4">
     <div class="card m-5">
-      <div class="card-body">
-        <div>
+        <div class="card-body">
+            <div>
 
-          <p>
-              Buen dia,<br>
+                <p>
+                    Buen dia,<br>
 
-              Me permito informar que la cita solicitada para el paciente {{$row[2]}}. Quedo asignada de la siguiente manera: <br>
-          </p>
-          <p>
-              Servicio: {{$row[15] }} <br>
-              Fecha: {{$row[8]}} <br>
-              Hora: {{$row[9]}} <br>
-              Profesional: {{$row[11] }} <br>
-              Sede: {{$row[32] }} <br>
-              Direccion: {{$row[33] }} <br>
-          </p>
-          <b>
-              Recuerde llevar la documentacion completa, feliz dia!!
-          </b>
-      </div>
-     </div>
+                    Me permito informar que la cita solicitada para el paciente {{$row[2]}}. Quedo asignada de la
+                    siguiente manera: <br>
+                </p>
+                <p>
+                    Servicio: {{$row[15] }} <br>
+                    Fecha: {{$row[8]}} <br>
+                    Hora: {{$row[9]}} <br>
+                    Profesional: {{$row[11] }} <br>
+                    Sede: {{$row[32] }} <br>
+                    Direccion: {{$row[33] }} <br>
+                </p>
+                <b>
+                    Se indica llegar 20 min antes para proceso de facturación, Debe llevar orden médica,
+                    historia clínica, copia del documento y autorización del servicio vigente y dirigida a Sireb.
+                </b>
+            </div>
+        </div>
     </div>
-   </div>
+</div>
 
-  <script src="/resources/js/script.js"></script>
-</x-app-layout>
+<script src="/resources/js/script.js"></script>
 
